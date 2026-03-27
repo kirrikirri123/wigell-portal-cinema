9b82491ee8648aabd41d31c988b10c593cad5ebb
Kristina Ahlenius <kristinaahlenius@gmail.com>
2026-03-27 09:28:14 +0100

    first commit

---
+++ src/main/java/com/ahlenius/wigell_cinema/WigellCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.ahlenius.wigell_cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WigellCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WigellCinemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahlenius/wigell_cinema/WigellCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahlenius.wigell_cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WigellCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
