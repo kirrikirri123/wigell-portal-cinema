d15cd6ff945828ea6884e0cb9f0ad5f2157bbf6d
Kristina Ahlenius <kristinaahlenius@gmail.com>
2026-04-03 15:21:56 +0200

    updated validation on createMovieDto

--- src/main/java/com/ahlenius/wigell_cinema/dto/movieDto/CreateMovieDto.java
+++ src/main/java/com/ahlenius/wigell_cinema/dto/movieDto/CreateMovieDto.java
@@ -2,16 +2,13 @@ package com.ahlenius.wigell_cinema.dto.movieDto;
 
 import com.ahlenius.wigell_cinema.model.AgeLimit;
 import com.ahlenius.wigell_cinema.model.Genre;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public record CreateMovieDto(
         @NotBlank @Size(min =2 , max=50) String title,
-        @NotBlank @Size(max=15) Genre genre,
-        @NotBlank @Min(5)@Max(360) int durationMin,
-        @NotBlank @Size(max = 7)AgeLimit ageLimit
+        @NotNull Genre genre,
+        @NotNull @Min(5)@Max(360) int durationMin,
+        @NotNull AgeLimit ageLimit
 
 ) {
 }
