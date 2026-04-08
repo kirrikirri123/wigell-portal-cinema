4825f87d86ecf01df935e57e146b67806830e4f6
Kristina Ahlenius <kristinaahlenius@gmail.com>
2026-04-08 15:36:22 +0200

    fixed type from entity to var on findBy.. methods

--- src/main/java/com/ahlenius/wigell_cinema/exception/ErrorResponse.java
+++ src/main/java/com/ahlenius/wigell_cinema/exception/ErrorResponse.java
@@ -6,14 +6,16 @@ import java.time.LocalDateTime;
 
 public record ErrorResponse(
         String timestamp,
-        String status,
+        int status,
         String error,
         String message
+//Borde man få in path här också???
+
 ) {
     public static ErrorResponse of(HttpStatus status, String error, String message) {
         return new ErrorResponse(
                 LocalDateTime.now().toString(), // skapas 'automatiskt' så därför behöver inte läggas in i konstruktorn.
-                status.toString(), // gör om från httpstatus i barn exceptions till string.
+                status.value(),
                 error,
                 message);
     }
